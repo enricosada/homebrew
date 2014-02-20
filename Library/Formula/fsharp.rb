d3c0b4e2df4036000b0e5cf44626fabddd0aa94f
Enrico Sada <enrico@sada.io>
2014-02-20 01:48:45 +0100

    add fsharp formula

---
+++ Library/Formula/fsharp.rb
@@ -0,0 +1,21 @@
+require "formula"
+
+class Fsharp < Formula
+  homepage "http://fsharp.org/"
+
+  url "https://github.com/fsharp/fsharp/archive/3.1.1.1.tar.gz"
+  sha1 "7b83606f03e651d342520e17e23c3dbf18dc942f"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+
+    system "make", "install"
+  end
+
+  test do
+    `system "#{bin}/fsharpc", "--help"`
+  end
+end
